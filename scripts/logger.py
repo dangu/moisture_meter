16d65b3536e65ccdf5556d3693416bd5051133a9
Daniel Gullberg <daniel_gullberg@hotmail.com>
2016-05-21 12:52:19 +0200

    Now logging two samples to file

--- scripts/logger.py
+++ scripts/logger.py
@@ -1,30 +1,44 @@
 #!/usr/bin/python
 import serial
 import time
-import sys
+import logging
+
+info = logging.getLogger(__name__).info
 
 class Logger:
-    def __init__(self, portName):
+    def __init__(self, portName, logfile):
         """Init method"""
         self.ser = serial.Serial(port=portName)
-        print self.ser
         self.ser.timeout=2 # [s] timeout
-     
+ 
+        # configure logging
+        self.log = logging.basicConfig(level=logging.INFO,
+                        filename=logfile, # log to this file
+                        format='%(asctime)s %(message)s') # include timestamp
+
+ 
+
     def start(self):
         """Start logging"""
-        for i in range(10):
+        for i in range(3):
             self.ser.write("read\n")
             self.ser.baudrate =9600
             resp=self.ser.readlines()
-            print "Data: "
-            print resp
-    
+            # Do not log empty data
+            if resp != '':
+                logString=""
+                for data in resp:
+                    logString += data.strip() + ";"
+                    logString = logString[:-1]
+                print repr(logString)
+                info(logString)
+                
     def close(self):
         self.ser.close()
 
 
 if __name__=="__main__":
-    myLogger = Logger("/dev/ttyUSB0")
+    myLogger = Logger("/dev/ttyUSB0", "room.log")
     myLogger.start()
     myLogger.close()
 
