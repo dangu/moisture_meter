d1e55752d92604f3c58e1cd3399c54cf4f0a6f8a
Daniel Gullberg <daniel_gullberg@hotmail.com>
2016-05-21 13:40:12 +0200

    Now logging better to file, all sensors

--- scripts/logger.py
+++ scripts/logger.py
@@ -2,8 +2,10 @@
 import serial
 import time
 import logging
+import traceback
 
 info = logging.getLogger(__name__).info
+error = logging.getLogger(__name__).error
 
 class Logger:
     def __init__(self, portName, logfile):
@@ -14,24 +16,45 @@ class Logger:
         # configure logging
         self.log = logging.basicConfig(level=logging.INFO,
                         filename=logfile, # log to this file
-                        format='%(asctime)s %(message)s') # include timestamp
+                        format='%(asctime)s;%(levelname)s;%(message)s') # include timestamp
 
  
 
     def start(self):
         """Start logging"""
-        for i in range(3):
-            self.ser.write("read\n")
-            self.ser.baudrate =9600
-            resp=self.ser.readlines()
-            # Do not log empty data
-            if resp != '':
-                logString=""
-                for data in resp:
-                    logString += data.strip() + ";"
+        sampleTime=3 #[s]
+        oldtime=time.time()-sampleTime
+        timeToSleep=sampleTime
+        oldTimeToSleep = timeToSleep
+        while(True):
+            try:
+                realDelay=(time.time()-oldtime)
+                operationTime = realDelay-oldTimeToSleep
+                #print "Operation time: %g" %operationTime
+                timeToSleep =sampleTime-operationTime
+                #print "Real delay: %g" %(realDelay)
+                oldtime=time.time()
+                oldTimeToSleep = timeToSleep
+                #print "Time to sleep: %g" %timeToSleep
+                        
+                time.sleep(max(0, timeToSleep)) # Sleeping this time
+                self.ser.write("read\n")
+                self.ser.baudrate =9600
+                resp=self.ser.readlines()
+                # Do not log empty data
+                if resp != '':
+                    logString=""
+                    for data in resp:
+                        logString += data.strip() + ";"
                     logString = logString[:-1]
-                print repr(logString)
-                info(logString)
+                    info(logString)
+            except KeyboardInterrupt:
+                break
+            except:
+                error("Error 1")
+                error(traceback.format_exc())
+                error("Error 2")
+        
                 
     def close(self):
         self.ser.close()
